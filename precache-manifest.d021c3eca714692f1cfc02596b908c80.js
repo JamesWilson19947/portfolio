d52beeb252d8e2837268dfd19dfeb074e3e6a2b9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-03 16:40:41 +0000

    Deploy JamesWilson19947/JamesWilson19947.github.io to github.com/JamesWilson19947/JamesWilson19947.github.io.git:master

---
+++ precache-manifest.d021c3eca714692f1cfc02596b908c80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19ae395c00adad1f47e1309f7ad4f346",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7170d72787fbb8c2b5b",
+    "url": "/static/css/main.ceb00faf.chunk.css"
+  },
+  {
+    "revision": "fdbe7f1d24694a760813",
+    "url": "/static/js/2.a69c2946.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.a69c2946.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e7170d72787fbb8c2b5b",
+    "url": "/static/js/main.73b7445e.chunk.js"
+  },
+  {
+    "revision": "018d5e63d6d7ade2f311",
+    "url": "/static/js/runtime-main.e1f21542.js"
+  },
+  {
+    "revision": "92dc6217c1a508074fa12e4e4bc88208",
+    "url": "/static/media/me.92dc6217.png"
+  }
+]);
